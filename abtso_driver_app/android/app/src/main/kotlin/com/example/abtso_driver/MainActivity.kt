487f81d9a64b25bab9ab78b08a6ff068916a40a2
oscarzgo1 <164500562+oscarzgo1@users.noreply.github.com>
2026-07-14 16:17:12 +0100

    Delete abtso_driver_app directory

--- abtso_driver_app/android/app/src/main/kotlin/com/example/abtso_driver/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.abtso_driver
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- abtso_driver_app/test/widget_test.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-
-void main() {
-  test('App basic unit test wrapper', () {
-    expect(true, isTrue);
-  });
-}
